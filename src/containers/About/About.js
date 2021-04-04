3304ac5281174a48f63bd849f29cc33eff7c807f
Naiara <naiaraplatero@gmail.com>
2021-04-05 01:00:54 +0200

    Version_1

---
+++ src/containers/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>Sobre nosotros</h1>
+                <p>About us</p>
+            </>
+        )
+    }
+}
+
+export default About;
